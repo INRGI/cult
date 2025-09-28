011a14debd0c5dfd25d2045008bbd4e058d77a45
Ruslan <120032162+INRGI@users.noreply.github.com>
2025-09-28 17:59:57 +0200

    repository import

--- apps/backend/src/app/modules/seed/seed.module.ts
+++ apps/backend/src/app/modules/seed/seed.module.ts
@@ -5,9 +5,10 @@ import {
   serviceProviders,
 } from "./seed.providers";
 import { SeedRepositoryModule } from "../../infrastructure/database/repositories/seed/seed.repository.module";
+import { AccountRepositoryModule } from "../../infrastructure/database/repositories/account/account.repository.module";
 
 @Module({
-  imports: [SeedRepositoryModule],
+  imports: [SeedRepositoryModule, AccountRepositoryModule],
   controllers: [...messageControllers],
   providers: [...serviceProviders, ...queryProviders],
   exports: [...serviceProviders, ...queryProviders],
